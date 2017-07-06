8b2fbc48d85db374487df3c895010cd181dbfb91
bhargava-somineni <somineni3@gmail.com>
2017-07-05 22:41:04 -0400

    Update videos list based on search input, with a throttle on search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -20,13 +20,17 @@ class SearchBar extends Component {
             //above is bad, because react will unable to know that state is changed to new value
             //always modify the components value using this.setState();
             <div className="search-bar">
-                <input 
-                value={this.state.term} 
-                onChange={event => this.setState({ term: event.target.value })} />
+                <input
+                    value={this.state.term}
+                    onChange={event => this.onInputChange(event.target.value)} />
             </div>
         );
     }
 
+    onInputChange(term) {
+        this.setState({ term });
+        this.props.onSearchTermChange(term);
+    }
 
     //alternative rendering 
     //==========================================//
